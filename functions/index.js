900d242a5d4ee9d9d4381ffc70853c1718ee943b
SecWind <wisanu.mywork@gmail.com>
2018-09-11 16:50:10 +0700

    of The End Section 22

--- functions/index.js
+++ functions/index.js
@@ -2,26 +2,33 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+
 const newActivity = (type, event, id) => {
     return {
-            type: type,
-            eventDate: event.date,
-            hostedBy: event.hostedBy,
-            title: event.title,
-            photoURL: event.hostphotoURL,
-            timestamp: admin.firestore.FieldValue.serverTimestamp(),
-            hostUid: event.hostUid,
-            eventId: id
+        type: type,
+        eventDate: event.date,
+        hostedBy: event.hostedBy,
+        title: event.title,
+        photoURL: event.hostphotoURL,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        hostUid: event.hostUid,
+        eventId: id
     }
 }
 
+
+//functions.firestore.document('events/{eventId}').onCreate คือการตรวจจับ func onCreate โดย document('events/{eventId}') คือ Collection 'events' และ /{eventId} แปลว่า Doc id  ถ้ามี ('events/{eventId}/number') แปลว่า ให้ตรวจจับ func onCreate Collection 'events' และidที่สร้างใน ในข้อมูลของ number
 exports.createActivity = functions.firestore.document('events/{eventId}')
     .onCreate(event => {
         let newEvent = event.data();
 
         console.log(newEvent);
 
-        const activity = newActivity('newEvent', newEvent, event.id )
+        const activity = newActivity('newEvent', newEvent, event.id)
 
         console.log(activity)
 
@@ -37,22 +44,24 @@ exports.createActivity = functions.firestore.document('events/{eventId}')
 
 exports.cancelActivity = functions.firestore.document('events/{eventId}')
     .onUpdate((event, context) => {
-    let updatedEvent = event.after.data()
-    let previousEventData = event.before.data()
-    console.log(event)
-    console.log(context)
-    console.log(updatedEvent)
-    console.log(previousEventData)
+        let updatedEvent = event.after.data()
+        let previousEventData = event.before.data()
+        console.log(event)
+        console.log(context)
+        console.log(updatedEvent)
+        console.log(previousEventData)
 
-    if (!updatedEvent.cancelled || updatedEvent.cancelled === previousEventData.cancelled) return false
+        if (!updatedEvent.cancelled || updatedEvent.cancelled === previousEventData.cancelled) return false
 
 
-    //  newActivity = func send     type ,   data.  id
-    const activity = newActivity('cancleedEvent', updatedEvent, context.params.eventId)
+        //  newActivity = func send     type ,   data.  id
+        const activity = newActivity('cancelledEvent', updatedEvent, context.params.eventId)
 
-    console.log({activity})
+        console.log({
+            activity
+        })
 
-    return admin.firestore().collection('activity')
+        return admin.firestore().collection('activity')
             .add(activity)
             .then((docRef) => {
                 return console.log('Activety created with ID: ', docRef.id);
@@ -61,4 +70,39 @@ exports.cancelActivity = functions.firestore.document('events/{eventId}')
                 return console.log('Error activety', err);
             })
 
-})    
+    })
+
+
+
+// นำพารามิเตอร์ที่ส่งเขามาที่ HTTP endpoint ไป Insert เข้าไปใน 
+// Realtime Database ที่ path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // รับค่า text พารามิเตอร์ที่ส่งเข้ามา
+    const original = req.query.text;
+    const id = req.query.id;
+    // insert เข้าไปใน Realtime Database แล้วส่ง response
+        return admin.database().ref('/messages').push({
+        original: original,
+        idName: id
+    }).then(snapshot => {
+        // รีไดเรค (ด้วย code 303)ไปที่ url ของ Firebase console เพื่อดูข้อมูลที่เพิ่มเข้าไป
+        return res.redirect(303, snapshot.ref);
+    });
+    // url คือ https://us-central1-revents-3aac5.cloudfunctions.net/addMessage
+    // url ทำ req https://us-central1-revents-3aac5.cloudfunctions.net/addMessage?text=uppercaseme
+});
+
+
+// ถ้ามี(Listener) onCreate หรือ การสร้างข้อมูลใหม่บน RTD ให้เกิด func makeUppercase
+exports.makeUppercase = functions.database.ref('/messages/{dataId}/idName')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.dataId, original);
+
+      //สร้างตัวแปร uppercase เท่ากับ ข้อมูลของ '/messages/{pushId}/idName' กลายเป็นตัวใหญ่ทั้งหมด
+      const uppercase = original.toUpperCase();
+
+      //return ข้อมูล ให้สร้าง upperData: -- uppercase -> ข้อมูลของ idName ที่กลายเป็นตัวใหญ่ทั้งหมด
+      return snapshot.ref.parent.child('upperData').set(uppercase);
+    });
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,106 +1,115 @@
-import React, { Component } from 'react'
-import { Grid, Loader} from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Grid, Loader } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
-import { connect } from 'react-redux'
-import { firestoreConnect} from 'react-redux-firebase'
-import { getEventsForDashboard } from '../eventActions'
-import LoadingComponent from '../../../app/layout/LoadingComponent'
-import EventActivity from '../EventActivity/EventActivity'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { getEventsForDashboard } from '../eventActions';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import EventActivity from '../EventActivity/EventActivity';
 
+const query = [
+  {
+    collection: 'activity',
+    orderBy: ['timestamp', 'desc'],
+    limit: 5
+  }
+];
 
-const mapState = (state) => ({
+const mapState = state => ({
   events: state.events,
-  loading: state.async.loading
-})
+  loading: state.async.loading,
+  activities: state.firestore.ordered.activity
+});
 
 const actions = {
   getEventsForDashboard
-}
+};
 
 class EventDashboard extends Component {
-    state = {
-      moreEvents : false,
-      initialLoading: true,
-      newEvent: []
-    }
+  state = {
+    moreEvents: false,
+    initialLoading: true,
+    newEvent: [],
+    context: {}
+  };
 
-    // DeleteEvents = (eveniId) => () => {
-    //   this.props.deleteEvent(eveniId);
-      
-    // }
-    async componentDidMount() {
-    let next =  await this.props.getEventsForDashboard()
+  // DeleteEvents = (eveniId) => () => {
+  //   this.props.deleteEvent(eveniId);
+
+  // }
+  async componentDidMount() {
+    let next = await this.props.getEventsForDashboard();
     // console.log(next);
 
-      // ถ้า func getEventsForDashboard มีข้อมูลส่งมามากกว่า 1 ตัว ให้ setState
-      if (next && next.docs && next.docs.length > 1) {
-        this.setState({
-          moreEvents: true,
-          initialLoading: false
-        })
-      }
+    // ถ้า func getEventsForDashboard มีข้อมูลส่งมามากกว่า 1 ตัว ให้ setState
+    if (next && next.docs && next.docs.length > 1) {
+      this.setState({
+        moreEvents: true,
+        initialLoading: false
+      });
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-      // เริ่มต้น this.props.events จะได้เป็น [] เสมอ //nextProps.events คือข้อมูลที่ได้จากการ query firestore
-        if (this.props.events !== nextProps.events) {
-      // เงื่อนไขคือให้ push this.state.newEvent ไปเรื่อยๆด้วยเอาข้อมูลมาเพิ่มจาก ...nextProps.events    
-          this.setState({
-            newEvent: [...this.state.newEvent, ...nextProps.events]
-          })
-        }
+  componentWillReceiveProps(nextProps) {
+    // เริ่มต้น this.props.events จะได้เป็น [] เสมอ //nextProps.events คือข้อมูลที่ได้จากการ query firestore
+    if (this.props.events !== nextProps.events) {
+      // เงื่อนไขคือให้ push this.state.newEvent ไปเรื่อยๆด้วยเอาข้อมูลมาเพิ่มจาก ...nextProps.events
+      this.setState({
+        newEvent: [...this.state.newEvent, ...nextProps.events]
+      });
     }
+  }
 
-    getNextEvents = async () => {
-      const {events, getEventsForDashboard} = this.props
-      // lastEvent คือการ get ข้อมูลตัวสุดท้าย แล้วเอา lastEvent ไปทำ func statAfter query อีกที
-      let lastEvent = events && events[events.length -1]
-      // console.log(lastEvent);
-      let next = await getEventsForDashboard(lastEvent)
-      console.log(next);
-      if (next && next.docs && next.docs.length <= 1) {
-        this.setState({
-          moreEvents: false
-        })
-      }
-      
+  getNextEvents = async () => {
+    const { events, getEventsForDashboard } = this.props;
+    // lastEvent คือการ get ข้อมูลตัวสุดท้าย แล้วเอา lastEvent ไปทำ func statAfter query อีกที
+    let lastEvent = events && events[events.length - 1];
+    // console.log(lastEvent);
+    let next = await getEventsForDashboard(lastEvent);
+    // console.log(next);
+    if (next && next.docs && next.docs.length <= 1) {
+      this.setState({
+        moreEvents: false
+      });
     }
+  };
 
-
+  handleContextRef = contextRef => this.setState({contextRef})
 
   render() {
-     const { loading } = this.props;
-     const { newEvent, moreEvents } = this.state
-     if (this.state.initialLoading) {
-        return <LoadingComponent inverted={true}/>
-     }
-    
+    const { loading, activities } = this.props;
+    const { newEvent, moreEvents } = this.state;
+    if (this.state.initialLoading) {
+      return <LoadingComponent inverted={true} />;
+    }
+
     return (
       <Grid>
-            <Grid.Column width={10}>
-              <EventList 
-                events={newEvent}
-                loading={loading}
-                moreEvents={moreEvents}
-                getNextEvents={this.getNextEvents}
-              />
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <EventActivity/>
-            </Grid.Column> 
-            <Grid.Column width={10}>
-             <Loader active={loading}/>
-              </Grid.Column> 
+        <Grid.Column width={10}>
+        <div ref={this.handleContextRef}>
+          <EventList
+            events={newEvent}
+            loading={loading}
+            moreEvents={moreEvents}
+            getNextEvents={this.getNextEvents}
+          />
+        </div>
+          
+        </Grid.Column>
+
+        <Grid.Column width={6}>
+          <EventActivity activities={activities} contextRef={this.state.contextRef} />
+        </Grid.Column>
+
+        <Grid.Column width={10}>
+          <Loader active={loading} />
+        </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
 
-export default 
-  connect(mapState, actions)(
-    firestoreConnect([{ collection: 'events' }])(EventDashboard)
-  )
-  
-  
-
-
+export default connect(
+  mapState,
+  actions
+)(firestoreConnect(query)(EventDashboard));
